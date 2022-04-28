ec4ecfc87b006ab2a3b3d00100728a48e836a60e
snehalshirsath <snehalshirs@gmail.com>
2022-04-28 13:04:57 +0100

    cidr updated

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -22,7 +22,7 @@ variable "eks-subnet-1-az" {
 }
 
 variable "subnet-2-cidr-block" {
-    default = "10.0.1.0/20"
+    default = "10.1.0.0/20"
 }
 
 variable "subnet-2-tag-name" {
@@ -34,7 +34,7 @@ variable "eks-subnet-2-az" {
 }
 
 variable "subnet-3-cidr-block" {
-    default = "10.0.2.0/20"
+    default = "10.2.0.0/20"
 }
 
 variable "subnet-3-tag-name" {
@@ -50,5 +50,5 @@ variable "eks-cluster-sg-name" {
 }
 
 variable "eks-sg-cidr-blocks" {
-    default = "['0.0.0.0/0']"
+    default = "0.0.0.0/0"
 }
