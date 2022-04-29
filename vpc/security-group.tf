2d6479cae552e76bcffbe7c346d4407ec752ac4f
snehalshirsath <snehalshirs@gmail.com>
2022-04-29 17:18:50 +0100

    files updated

--- vpc/security-group.tf
+++ vpc/security-group.tf
@@ -36,6 +36,48 @@ resource "aws_security_group" "eks-allnodes-sg" {
   }
 }
 
+
+resource "aws_security_group" "bastion-ec2-instance-security-group" {
+    name        = var.bastion-host-sg-name
+    description = "Allow HTTP, HTTPS, and SSH"
+    vpc_id = var.eks-vpc-id
+
+    // HTTP
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    // HTTPS
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    // SSH
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+output "security-group-id" {
+  value = aws_security_group.bastion-ec2-instance-security-group.id
+}
+
 output "eks-allnodes-sg" {
   value = aws_security_group.eks-allnodes-sg.id
 }
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,6 +1,50 @@
 
 #variable "bastion-host-sg-id" {}
 
+
+
+
+variable "eks-vpc-id" {}
+
+
+variable "image-id" {
+    description = "image id for instance"
+    default = "ami-003f91f482a604b6d"
+}
+
+variable "instance-type" {
+    description = "instance type for autoscaling group"
+    default = "t2.medium"
+}
+
+variable "availability-zone" {
+    default = "eu-west-1c"
+}
+
+variable "bastion-ec2-instance" {
+    default = "eks-bastion-EC2-instance"
+}
+
+variable "ec2-instance-key-name" {
+    default = "attraqt-snehal-aws-key"
+}
+
+variable "bastion-host-sg-name" {
+    default = "eks-bastion-host-sg"
+}
+
+variable "subnet-3-cidr-block" {
+    default = "172.31.16.0/20"
+}
+
+variable "subnet-3-tag-name" {
+    default = "eks-vpc-subnet-3-attraqt"
+}
+
+variable "eks-subnet-3-az" {
+    default = "eu-west-1c"
+}
+
 variable "eks-cluster-name" {
     default = "eks-cluster-attraqt"
 }
