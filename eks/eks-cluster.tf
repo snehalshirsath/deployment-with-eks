c1b2e689ec7000a9622040eddbf2f47583b25efb
snehalshirsath <snehalshirs@gmail.com>
2022-04-30 15:20:44 +0100

    data updated

--- eks/eks-cluster.tf
+++ eks/eks-cluster.tf
@@ -4,35 +4,6 @@
 #  * EC2 Security Group to allow networking traffic with EKS cluster
 #  * EKS Cluster
 #  
-
-resource "aws_iam_openid_connect_provider" "eks-iam-connect-provider" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.example.certificates[0].sha1_fingerprint]
-  url             = aws_eks_cluster.eks-cluster-attraqt.identity[0].oidc[0].issuer
-}
-
-data "aws_iam_policy_document" "eks-attraqt-assume-role-policy" {
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    effect  = "Allow"
-
-    condition {
-      test     = "StringEquals"
-      variable = "${replace(aws_iam_openid_connect_provider.eks-iam-connect-provider.url, "https://", "")}:sub"
-      values   = ["system:serviceaccount:kube-system:aws-node"]
-    }
-    principals {
-      identifiers = [aws_iam_openid_connect_provider.eks-iam-connect-provider.arn]
-      type        = "Federated"
-    }
-  }
-}
-
-resource "aws_iam_role" "eks-service-iam" {
-  assume_role_policy = data.aws_iam_policy_document.eks-attraqt-assume-role-policy.json
-  name               = "eks-iam-service-account-role"
-}
-
 resource "aws_iam_role" "eks-attraqt-cluster" {
   name = var.eks-cluster-role-name
 
@@ -63,6 +34,9 @@ resource "aws_iam_role_policy_attachment" "eks-attraqt-cluster-AmazonEKSVPCResou
   role       = aws_iam_role.eks-attraqt-cluster.name
 }
 
+#
+# eks cluster resource
+#
 resource "aws_eks_cluster" "eks-cluster-attraqt" {
   name     = var.eks-cluster-name
   role_arn = aws_iam_role.eks-attraqt-cluster.arn
@@ -80,6 +54,13 @@ resource "aws_eks_cluster" "eks-cluster-attraqt" {
   ]
 }
 
+output "attraqt-eks-cluster-name" {
+  value= aws_eks_cluster.eks-cluster-attraqt.name
+}
+
+output "eks-cluster-endpoint" {
+  value = aws_eks_cluster.eks-cluster-attraqt.endpoint
+}
 
 resource "kubernetes_config_map" "aws_auth_configmap" {
   metadata {
@@ -101,14 +82,40 @@ YAML
   }
 }
 
-output "eks-cluster-endpoint" {
-  value = aws_eks_cluster.eks-cluster-attraqt.endpoint
+data "tls_certificate" "eks-cluster-tls" {
+  url = aws_eks_cluster.eks-cluster-attraqt.identity[0].oidc[0].issuer
 }
+
+resource "aws_iam_openid_connect_provider" "eks-iam-connect-provider" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks-cluster-tls.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.eks-cluster-attraqt.identity[0].oidc[0].issuer
+}
+
+data "aws_iam_policy_document" "eks-attraqt-assume-role-policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks-iam-connect-provider.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-node"]
+    }
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.eks-iam-connect-provider.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "eks-service-iam" {
+  assume_role_policy = data.aws_iam_policy_document.eks-attraqt-assume-role-policy.json
+  name               = "eks-iam-service-account-role"
+}
+
 
 output "kubeconfig-certificate-authority-data" {
   value = aws_eks_cluster.eks-cluster-attraqt.certificate_authority[0].data
 }
 
-output "attraqt-eks-cluster-name" {
-  value= aws_eks_cluster.eks-cluster-attraqt.name
-}
