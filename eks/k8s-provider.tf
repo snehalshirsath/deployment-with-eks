192e5515fefe3d0c79fe8f7ecd003c00bdd7c31e
snehalshirsath <snehalshirs@gmail.com>
2022-04-30 03:29:59 +0100

    k8s provider, aws_auth configmap added

--- eks/k8s-provider.tf
+++ eks/k8s-provider.tf
@@ -11,5 +11,4 @@ provider "kubernetes" {
   host                   = aws_eks_cluster.eks-cluster-attraqt.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster-attraqt.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster_auth.token
-  load_config_file       = false
 }
