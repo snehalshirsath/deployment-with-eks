81e4ba12774884eb6377b613162874d46731a649
snehalshirsath <snehalshirs@gmail.com>
2022-05-01 13:03:26 +0100

    data added

--- eks/eks-worker-nodes.tf
+++ eks/eks-worker-nodes.tf
@@ -51,10 +51,36 @@ resource "aws_eks_node_group" "eks-ng-attraqt" {
     min_size     = 2
   }
 
+  
+data "aws_ecr_authorization_token" "token" {
+}
+
+resource "kubernetes_secret" "docker" {
+  metadata {
+    name      = "docker-cfg"
+    namespace = "kube-system"
+  }
+
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        "${data.aws_ecr_authorization_token.token.proxy_endpoint}" = {
+          auth = "${data.aws_ecr_authorization_token.token.authorization_token}"
+        }
+      }
+    })
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+}
+
   depends_on = [
+    kubernetes_secret.docker,
     aws_iam_role_policy_attachment.eks-attraqt-node-AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.eks-attraqt-node-AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.eks-attraqt-node-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
+
+
 
